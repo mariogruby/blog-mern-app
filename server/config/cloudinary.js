a49f969a8cc5f3ddd2150fb95ee44a091c1b618b
mario ruby <mariogabrielrn@gmail.com>
2025-01-22 19:03:11 +0100

    npm install

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import multer from 'multer';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// import multer from 'multer';
 
 cloudinary.config({
     cloud_name: 'ddg9b0qik',
