8aaa3b2db03a6105389a04954be9692a72cac243
mario ruby <mariogabrielrn@gmail.com>
2025-01-22 19:34:39 +0100

    act mongo

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blog-app"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blog-app"
 
 mongoose
-    .connect(MONGO_URI)
+    .connect(MONGODB_URI)
     .then((x) => {
         const dbName = x.connections[0].name;
         console.log(dbName);
